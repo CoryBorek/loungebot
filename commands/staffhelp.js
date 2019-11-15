867f9d57ec1d644ae1b409bc337234a050fc2967
NotJishy <jishydoods@gmail.com>
2019-11-15 18:06:58 -0500

    Added restart to staffhelp

--- commands/staffhelp.js
+++ commands/staffhelp.js
@@ -17,7 +17,8 @@ exports.staffhelp = async (msg, Discord, config, blue, red) => {
             .addField("- " + (config.prefix) + "mute (CURRENTLY BROKEN)", "`Mute a user for specified amount of time. Format: *" + (config.prefix) + "mute @user <time> <reason>*`", true)
             .addField("- " + (config.prefix) + "purge", "`Mass delete any amount of messages at once. Format: *" + (config.prefix) + "purge <amount>*`", true)
             .addField("- " + (config.prefix) + "announce", "`Send an announcement to the announcement channel. Format: *" + (config.prefix) + "announce <tag> <everyone | here | false> <message>*`", true)
-            .addField("- " + (config.prefix) + "joinmsg", "`Edit and view the welcome message for new users.`", true);
+            .addField("- " + (config.prefix) + "joinmsg", "`Edit and view the welcome message for new users.`", true)
+            .addField("- " + (config.prefix) + "restart", "`Restarts the bot. ***(DO NOT USE WITHOUT PERMISSION)*** Format: *" + (config.prefix) + "restart*`", true);
         msg.channel.send(shEmbed);
     }
 }
