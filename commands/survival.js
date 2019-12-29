2ede96c1cabd06a64cf113f7b64a5932b680b054
NotJishy <jishydoods@gmail.com>
2019-12-29 15:52:50 -0500

    Updated Lounge Survival info

--- commands/survival.js
+++ commands/survival.js
@@ -2,8 +2,8 @@ exports.survival = function (Discord, msg, blue) {
     const infoEmbed = new Discord.RichEmbed()
         .setColor(blue)
         .setTitle("The Lounge Survival Information")
-        .addField("__Server IP:__", "**SurvivalLounge.aternos.me**", false)
-        .addField("__Version:__", "*1.14.4*", false)
+        .addField("__Server IP:__", "**TheLoungeDisc.aternos.me**", false)
+        .addField("__Version:__", "*1.15.1*", false)
         .addField("__Game:__", "*Minecraft Java Edition*", false);
     msg.channel.send(infoEmbed);
 }
